57c87fde3aca2cf424943d3487ffd0178f37e289
Jake Ratcliffe <JakeR1342@gmail.com>
2024-09-07 19:49:38 +0100

    Added model definitions and re-structured project

---
+++ domain/src/models/users.rs
@@ -0,0 +1,21 @@
+use crate::schema::users;
+use diesel::prelude::*;
+use rocket::serde::{Deserialize, Serialize};
+use std::cmp::{Eq, Ord, PartialEq, PartialOrd};
+
+#[derive(Queryable, Serialize, Ord, Eq, PartialOrd, PartialEq)]
+pub struct User {
+    pub id: i32,
+    pub name: String,
+    pub claan_id: i32,
+    pub active: bool,
+}
+
+#[derive(Insertable, Deserialize)]
+#[serde(crate = "rocket::serde")]
+#[diesel(table_name = users)]
+pub struct NewUser {
+    pub name: String,
+    pub claan_id: i32,
+    pub active: bool,
+}
