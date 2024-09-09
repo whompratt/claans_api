f1a8a637c305314f148d1621e527c80d1ed095b0
Jake Ratcliffe <JakeR1342@gmail.com>
2024-09-09 01:28:08 +0100

    Implemented update and delete for Claans

--- api/src/handlers/claans.rs
+++ api/src/handlers/claans.rs
@@ -1,9 +1,8 @@
-use application::claans::create;
-use application::claans::read;
+use application::claans::{create, delete, read, update};
 use domain::models::claans::{Claan, NewClaan};
 use rocket::response::status::{Created, NotFound};
 use rocket::serde::json::Json;
-use rocket::{get, post};
+use rocket::{delete, get, patch, post};
 use shared::response_models::{Response, ResponseBody};
 
 #[get("/claans")]
@@ -29,4 +28,24 @@ pub fn get_claan(claan_id: i32) -> Result<String, NotFound<String>> {
 #[post("/claans", format = "application/json", data = "<claan>")]
 pub fn create_claan(claan: Json<NewClaan>) -> Created<String> {
     create::create_claan(claan)
+}
+
+#[patch("/claans", format = "application/json", data = "<claan>")]
+pub fn update_claan(claan_id: i32, claan: Json<Claan>) -> Result<String, NotFound<String>> {
+    let claan = update::update_claan(claan)?;
+    let response = Response {
+        body: ResponseBody::Claan(claan),
+    };
+
+    Ok(serde_json::to_string(&response).unwrap())
+}
+
+#[delete("/claans/<claan_id>")]
+pub fn delete_claan(claan_id: i32) -> Result<String, NotFound<String>> {
+    let claan = delete::delete_claan(claan_id)?;
+    let response = Response {
+        body: ResponseBody::Claans(claan),
+    };
+
+    Ok(serde_json::to_string(&response).unwrap())
 }
