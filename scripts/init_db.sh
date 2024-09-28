741451938996f4a12069b45364b2069878899189
Jake Ratcliffe <JakeR1342@gmail.com>
2024-09-28 20:19:35 +0100

    More work on zerotoprod

---
+++ scripts/init_db.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+
+# Check sqlx-cli is installed
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error, sqlx not installed"
+  echo >&2 "Use:"
+  echo >&2 "    cargo install sqlx-cli --no-default-features --features rustls,postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
+
+# Check for set env vars else use default
+DB_PORT="${POSTGRES_PORT:=5432}"
+SUPERUSER="${SUPERUSER:=postgres}"
+SUPERUSER_PWD="${SUPERUSER_PWD:=password}"
+APP_USER="${APP_USER:=app}"
+APP_USER_PWD="${APP_USER_PWD:=secret}"
+APP_DB_NAME="${APP_DB_NAME:=claans}"
+
+# Launch postgres using docker
+CONTAINER_NAME="postgres"
+docker run \
+  --env POSTGRES_USER=${SUPERUSER} \
+  --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
+  --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
+  --health-interval=1s \
+  --health-timeout=5s \
+  --health-retries=5 \
+  --publish "${DB_PORT}":5432 \
+  --detach \
+  --name "${CONTAINER_NAME}" \
+  postgres -N 1000
+
+# Wait for ready
+until [ \
+  "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
+  "healthy" \
+]; do
+  >&2 echo "Postgres not ready yet, sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres ready on port ${DB_PORT}"
+
+# Create app user
+CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
+docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
+
+# Grante create privileges to app user
+GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
+docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
+
+# Init sqlx
+DATABASE_URL=postgres://${APP_USER}:${APP_USER_PWD}@localhost:${DB_POST}/${APP_DB_NAME}
+export DATABASE_URL
+sqlx database create
---
+++ tests/health_check.rs
@@ -0,0 +1,78 @@
+use std::net::TcpListener;
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = claans_api::run(listener).expect("Failed to bind port");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(&format!("{}/health_check", &app_address))
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to send request");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_for_missing_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing email"),
+        ("email=ursula_le_guin%40gmail.com", "missing name"),
+        ("", "missing name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to send request");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return 400 Bad Request when payload was {}",
+            error_message
+        );
+    }
+}
