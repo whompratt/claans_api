e0ff4ea7d1032ca868938d2a4db08b72e1173b84
Jake Ratcliffe <JakeR1342@gmail.com>
2024-09-28 20:19:35 +0100

    More work on zerotoprod

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-pub fn main() {
-    println!("Hello")
+use std::net::TcpListener;
+
+use claans_api::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    run(listener)?.await
 }
