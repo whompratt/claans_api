9b7e8eaa1a3d70fdb44911bf77dce174b0e4428d
Jake Ratcliffe <JakeR1342@gmail.com>
2024-10-01 01:22:08 +0100

    Completed telemetry section

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-use std::net::TcpListener;
-
+use claans_api::telemetry::{get_subscriber, init_subscriber};
 use claans_api::{configuration::get_configuration, startup::run};
+use secrecy::ExposeSecret;
+use sqlx::PgPool;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let configuration = get_configuration().expect("Failed to read configuration.yaml");
+    let subscriber = get_subscriber("claans_api".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
