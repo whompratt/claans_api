037ad657812d13830387743960247ef9e7419cc2
Jake Ratcliffe <JakeR1342@gmail.com>
2024-10-01 20:11:58 +0100

    Updated configurations

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,7 @@
 use claans_api::configuration::{get_configuration, DatabaseSettings};
 use claans_api::startup::run;
 use claans_api::telemetry::{get_subscriber, init_subscriber};
-use secrecy::{ExposeSecret, SecretString};
+use secrecy::SecretString;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use std::sync::LazyLock;
@@ -50,16 +50,15 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         password: SecretString::new("password".into()),
         ..config.clone()
     };
-    let mut connection =
-        PgConnection::connect(&maintenance_settings.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres");
+    let mut connection = PgConnection::connect_with(&maintenance_settings.connect_options())
+        .await
+        .expect("Failed to connect to Postgres");
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create database");
 
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(config.connect_options())
         .await
         .expect("Failed to connect to Postgres");
     sqlx::migrate!("./migrations")
