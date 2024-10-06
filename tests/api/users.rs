62ce03251669aaa43cdbe9a3161c8bd0a3a3eff6
Jake Ratcliffe <JakeR1342@gmail.com>
2024-10-06 15:22:11 +0100

    Updated confirmed to pending_confirmation for new users and regenerated queries

--- tests/api/users.rs
+++ tests/api/users.rs
@@ -3,7 +3,7 @@ use wiremock::matchers::{method, path};
 use wiremock::{Mock, ResponseTemplate};
 
 #[tokio::test]
-async fn register_returns_200_for_valid_form_data() {
+async fn register_returns_a_200_for_valid_form_data() {
     // Arrange
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -19,14 +19,32 @@ async fn register_returns_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+}
 
-    let saved = sqlx::query!("SELECT email, name FROM users",)
+#[tokio::test]
+async fn register_persists_the_new_subscriber() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    app.post_users(body.into()).await;
+
+    // Assert
+    let saved = sqlx::query!("SELECT email, name, status FROM users",)
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subcription");
 
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[tokio::test]
