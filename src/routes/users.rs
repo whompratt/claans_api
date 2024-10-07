aa213c15f54a959adea787e2a1e5635683a6c323
Jake Ratcliffe <JakeR1342@gmail.com>
2024-10-07 01:34:03 +0100

    Applied cargo formatting

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -129,12 +129,7 @@ pub async fn send_confirmation_email(
         confirmation_link
     );
     email_client
-        .send_email(
-            new_user.email,
-            "Welcome to Claans!",
-            html_body,
-            &plain_body,
-        )
+        .send_email(new_user.email, "Welcome to Claans!", html_body, &plain_body)
         .await
 }
 
