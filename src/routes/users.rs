5ee74eede063c2e1e3916ecec82ead3fbec5941e
Jake Ratcliffe <JakeR1342@gmail.com>
2024-10-07 21:18:38 +0100

    Changed new user flow to operate inside a db transaction
    Regenerate sqlx queries

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -1,7 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use rand::{distributions::Alphanumeric, thread_rng, Rng};
-use sqlx::PgPool;
+use sqlx::{Executor, PgPool, Postgres, Transaction};
 use uuid::Uuid;
 
 use crate::{
@@ -44,13 +44,22 @@ pub async fn register(
         Ok(user) => user,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-
-    let user_id = match insert_user(&pool, &new_user).await {
+    let mut transaction = match pool.begin().await {
+        Ok(transaction) => transaction,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+    let user_id = match insert_user(&mut transaction, &new_user).await {
         Ok(user_id) => user_id,
         Err(_) => return HttpResponse::InternalServerError().finish(),
     };
     let user_token = generate_user_token();
-    if store_token(&pool, user_id, &user_token).await.is_err() {
+    if store_token(&mut transaction, user_id, &user_token)
+        .await
+        .is_err()
+    {
+        return HttpResponse::InternalServerError().finish();
+    }
+    if transaction.commit().await.is_err() {
         return HttpResponse::InternalServerError().finish();
     }
     if send_confirmation_email(&email_client, new_user, &base_url.0, &user_token)
@@ -62,50 +71,12 @@ pub async fn register(
     HttpResponse::Ok().finish()
 }
 
-#[tracing::instrument(
-    name = "Store subscription token in the database",
-    skip(user_token, pool)
-)]
-pub async fn store_token(
-    pool: &PgPool,
-    user_id: Uuid,
-    user_token: &str,
-) -> Result<(), sqlx::Error> {
-    sqlx::query!(
-        r#"INSERT INTO user_tokens (user_token, user_id)
-        VALUES ($1, $2)"#,
-        user_token,
-        user_id
-    )
-    .execute(pool)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
-    Ok(())
-}
-
-#[tracing::instrument(name = "Saving new user to database", skip(new_user, pool))]
-pub async fn insert_user(pool: &PgPool, new_user: &NewUser) -> Result<Uuid, sqlx::Error> {
-    let user_id = Uuid::new_v4();
-    sqlx::query!(
-        r#"
-            INSERT INTO users (id, email, name, registered_at, status)
-            VALUES($1, $2, $3, $4, 'pending_confirmation')
-        "#,
-        user_id,
-        new_user.email.as_ref(),
-        new_user.name.as_ref(),
-        Utc::now()
-    )
-    .execute(pool)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
-    Ok(user_id)
+fn generate_user_token() -> String {
+    let mut rng = thread_rng();
+    std::iter::repeat_with(|| rng.sample(Alphanumeric))
+        .map(char::from)
+        .take(25)
+        .collect()
 }
 
 #[tracing::instrument(
@@ -133,10 +104,49 @@ pub async fn send_confirmation_email(
         .await
 }
 
-fn generate_user_token() -> String {
-    let mut rng = thread_rng();
-    std::iter::repeat_with(|| rng.sample(Alphanumeric))
-        .map(char::from)
-        .take(25)
-        .collect()
+#[tracing::instrument(name = "Saving new user to database", skip(transaction, new_user))]
+pub async fn insert_user(
+    transaction: &mut Transaction<'_, Postgres>,
+    new_user: &NewUser,
+) -> Result<Uuid, sqlx::Error> {
+    let user_id = Uuid::new_v4();
+    let query = sqlx::query!(
+        r#"
+            INSERT INTO users (id, email, name, registered_at, status)
+            VALUES($1, $2, $3, $4, 'pending_confirmation')
+        "#,
+        user_id,
+        new_user.email.as_ref(),
+        new_user.name.as_ref(),
+        Utc::now()
+    );
+    transaction.execute(query).await.map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(user_id)
+}
+
+#[tracing::instrument(
+    name = "Store subscription token in the database",
+    skip(transaction, user_token)
+)]
+pub async fn store_token(
+    transaction: &mut Transaction<'_, Postgres>,
+    user_id: Uuid,
+    user_token: &str,
+) -> Result<(), sqlx::Error> {
+    let query = sqlx::query!(
+        r#"
+            INSERT INTO user_tokens (user_token, user_id)
+            VALUES ($1, $2)
+        "#,
+        user_token,
+        user_id
+    );
+    transaction.execute(query).await.map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
