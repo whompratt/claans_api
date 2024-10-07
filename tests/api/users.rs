bb842535d9038015664531edfe2fd082b798a5d7
Jake Ratcliffe <JakeR1342@gmail.com>
2024-10-07 01:28:52 +0100

    Removed unecessary borrows

--- tests/api/users.rs
+++ tests/api/users.rs
@@ -132,6 +132,6 @@ async fn register_sends_a_confirmation_email_with_a_link() {
 
     // Assert
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let confirmation_links = app.get_confirmation_links(&email_request);
+    let confirmation_links = app.get_confirmation_links(email_request);
     assert_eq!(confirmation_links.html, confirmation_links.plain_text);
 }
