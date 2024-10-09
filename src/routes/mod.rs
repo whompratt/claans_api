30c29479d7fb08bf5bea563e8fb23125c3382949
Jake Ratcliffe <JakeR1342@gmail.com>
2024-10-10 00:18:25 +0100

    Expanded on error handling and output

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -5,3 +5,16 @@ mod users_confirm;
 pub use health_check::*;
 pub use users::*;
 pub use users_confirm::*;
+
+fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
+}
--- src/routes/users_confirm.rs
+++ src/routes/users_confirm.rs
@@ -1,27 +1,51 @@
-use actix_web::{web, HttpResponse};
+use actix_web::{http::StatusCode, web, HttpResponse, ResponseError};
+use anyhow::Context;
 use sqlx::PgPool;
 use uuid::Uuid;
+
+use super::error_chain_fmt;
 
 #[derive(serde::Deserialize)]
 pub struct Parameters {
     user_token: String,
 }
 
-#[tracing::instrument(name = "Confirm a pending user", skip(parameters, pool))]
-pub async fn confirm(parameters: web::Query<Parameters>, pool: web::Data<PgPool>) -> HttpResponse {
-    let id = match get_user_id_from_token(&pool, &parameters.user_token).await {
-        Ok(id) => id,
-        Err(_) => return HttpResponse::InternalServerError().finish(),
-    };
-    match id {
-        None => HttpResponse::Unauthorized().finish(),
-        Some(user_id) => {
-            if confirm_user(&pool, user_id).await.is_err() {
-                return HttpResponse::InternalServerError().finish();
-            }
-            HttpResponse::Ok().finish()
+#[derive(thiserror::Error)]
+pub enum ConfirmationError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+    #[error("There is not user associated with the provided token")]
+    UnknownToken,
+}
+
+impl std::fmt::Debug for ConfirmationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for ConfirmationError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            Self::UnknownToken => StatusCode::UNAUTHORIZED,
+            Self::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
+}
+
+#[tracing::instrument(name = "Confirm a pending user", skip(parameters, pool))]
+pub async fn confirm(
+    parameters: web::Query<Parameters>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, ConfirmationError> {
+    let user_id = get_user_id_from_token(&pool, &parameters.user_token)
+        .await
+        .context("Failed to retrieve user id associated with token provided")?
+        .ok_or(ConfirmationError::UnknownToken)?;
+    confirm_user(&pool, user_id)
+        .await
+        .context("Failed to update user status to 'confirmed'")?;
+    Ok(HttpResponse::Ok().finish())
 }
 
 #[tracing::instrument(name = "Mark user as confirmed", skip(user_id, pool))]
@@ -31,11 +55,7 @@ pub async fn confirm_user(pool: &PgPool, user_id: Uuid) -> Result<(), sqlx::Erro
         user_id,
     )
     .execute(pool)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
+    .await?;
     Ok(())
 }
 
@@ -50,10 +70,6 @@ pub async fn get_user_id_from_token(
         user_token,
     )
     .fetch_optional(pool)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
+    .await?;
     Ok(result.map(|r| r.user_id))
 }
